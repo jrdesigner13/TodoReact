0d085eee76116d7a1d1ce0f3bde83a1ce85c659c
antoniorealsymvolli <antonio.real@symvolli.com>
2021-11-04 10:20:48 +0000

    TypeScript

---
+++ src/App.tsx
@@ -0,0 +1,59 @@
+import {useState} from 'react';
+import * as C from './App.styles';
+import { Item } from './types/Item';
+import { ListItem } from './components/ListItem';
+import { AddArea } from './components/AddArea';
+
+const App = () => {
+  const [list, setList] = useState<Item[]>([
+    { id: 1, name: 'buy coffee', done: false },
+    { id: 2, name: 'buy beer', done: true},
+    { id: 3, name: 'go to the gym', done: true},
+  ]);
+
+  const handleAddTask = (taskName: string) => {
+    let newList = [...list];
+
+    newList.push({
+      id: list.length +1,
+      name: taskName,
+      done: false
+    });
+
+    setList(newList);
+  }
+
+  const handleTaskChange = (id: number, done: boolean) => {
+    let newList = [...list];
+    for(let i in newList) {
+      if(newList[i].id === id) {
+        newList[i].done = done;
+      }
+    }
+    setList(newList);
+  }
+
+  return (
+    <C.Container>
+      <C.Area>
+        <C.Header>To-do List</C.Header>
+
+        {/* Add new task */}
+        <AddArea onEnter={handleAddTask} />
+
+        {/* show the list itens */}
+        {list.map((item, index)=>(
+          <ListItem 
+            key={index} 
+            item={item} 
+            onChange={handleTaskChange}
+          />
+        ))}
+        </C.Area>
+    </C.Container>
+
+  );
+
+}
+
+export default App;
